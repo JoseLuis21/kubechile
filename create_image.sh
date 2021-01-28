ff0b13b9e1027040b61e86addc0e592692f223ae
jose9316 <josephluihs@gmail.com>
2021-01-28 17:42:37 -0300

    Proyecto Escalab

---
+++ create_image.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#AUTHENTICARSE CON AWSCLI
+aws configure --profile test < config.csv
+
+#AUTENTIFICARSE CON AWS ECR
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 769658871215.dkr.ecr.us-east-1.amazonaws.com
+
+#CREAR IMAGEN AWS ECR BACKEND
+docker build -t golangapp ./golangapp
+
+#TAGGEANDO LA APP BACKEND
+docker tag golangapp:latest 769658871215.dkr.ecr.us-east-1.amazonaws.com/golangapp:latest
+
+#PUSH DE LA IMAGEN BACKEND
+docker push 769658871215.dkr.ecr.us-east-1.amazonaws.com/golangapp:latest
+
+#CREAR IMAGEN AWS ECR FRONTEND
+docker build -t reactapp ./reactapp
+
+#TAGGEANDO LA APP FRONTEND
+docker tag reactapp:latest 769658871215.dkr.ecr.us-east-1.amazonaws.com/reactapp:latest
+
+#PUSH DE LA IMAGEN BACKEND
+docker push 769658871215.dkr.ecr.us-east-1.amazonaws.com/reactapp:latest
---
+++ reactapp/src/utils/constant.js
@@ -0,0 +1,2 @@
+export const API_HOST = "http://localhost:9090";
+export const TOKEN = "token";
